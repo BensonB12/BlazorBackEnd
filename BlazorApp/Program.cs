5401c83fa18bf0c518bc7a0e5c9ed60795410ec5
Benson Bird <benson.bird@students.snow.edu>
2023-10-16 09:19:22 -0600

    DBContext, floors page

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorApp.Data;
 using BlazorApp.Shared;
+using System.Text.Json.Serialization;
+using BlazorApp.DB_Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // [5] DEPENDANCY INJECTION BABY!!
@@ -10,9 +13,26 @@ builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<FloorService>();
 builder.Services.AddScoped<CounterState>();
+builder.Services.AddDbContext<DbBensonbird25Context>(
+    opt => opt.UseNpgsql(
+        builder.Configuration.GetConnectionString("db_string")
+        ));
+builder.Services.AddRazorPages().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
+
+//Not in production, just developement
+//await EnsureDatabaseIsMigrated(app.Services);
+
+async Task EnsureDatabaseIsMigrated(IServiceProvider s)
+{
+    using var scope = s.CreateScope();
+    using var ctx=scope.ServiceProvider.GetService<DbBensonbird25Context>();
+    if(ctx is not null)
+        await ctx.Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
